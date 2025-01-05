3472006d4edbb73c502163db06b664df62a8183e
abouthify <maarifalaawi@gmail.com>
2025-01-06 03:01:59 +0700

    first commit

---
+++ data/index.ts
@@ -0,0 +1,408 @@
+export const navItems = [
+  { name: "Home", link: "#satu" },
+  { name: "Projects", link: "#dua" },
+  { name: "Testimonials", link: "#testimonials" },
+  { name: "Contact", link: "#contact" },
+];
+
+export const gridItems = [
+  {
+    id: 1,
+    title: " I am someone who loves learning new things",
+    description: "",
+    className: "lg:col-span-3 md:col-span-6 md:row-span-4 lg:min-h-[60vh]",
+    imgClassName: "w-full h-full",
+    titleClassName: "justify-end",
+    img: "/alawi.svg",
+    spareImg: "",
+  },
+  {
+    id: 2,
+    title: "I am very open to collaboration, wherever you are.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 3,
+    title: "My tech stack",
+    description: "I constantly try to improve",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-2",
+    imgClassName: "",
+    titleClassName: "justify-center",
+    img: "",
+    spareImg: "",
+  },
+  {
+    id: 4,
+    title: "Data science enthusiast with a passion for development.",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-start",
+    img: "/grid.svg",
+    spareImg: "/b4.svg",
+  },
+
+  {
+    id: 5,
+    title: "Currently building data science library with Python",
+    description: "The Inside Scoop",
+    className: "md:col-span-3 md:row-span-2",
+    imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
+    titleClassName: "justify-center md:justify-start lg:justify-center",
+    img: "/b5.svg",
+    spareImg: "/grid.svg",
+  },
+  {
+    id: 6,
+    title: "Do you want to start a project together?",
+    description: "",
+    className: "lg:col-span-2 md:col-span-3 md:row-span-1",
+    imgClassName: "",
+    titleClassName: "justify-center md:max-w-full max-w-60 text-center",
+    img: "",
+    spareImg: "",
+  },
+];
+
+export const projects = [
+  {
+    id: 1,
+    title: "3🎯 E-commerce Websites",
+    des: "Online stores for buying and selling various products or services.",
+    img: "/1.svg",
+    iconLists: ["/JavaScript.svg", "/Python.svg", "/Ruby.svg", "/PHP.svg", "/Java.svg"],
+    link: "SC[IndiaNIC_Infotech]",
+  },
+  {
+    id: 2,
+    title: "4🎯 Business Websites",
+    des: "Information about company services, products, contact details, and more.",
+    img: "/2.svg",
+    iconLists: ["/HTML.svg", "/Python.svg", "/Ruby.svg", "/PHP.svg", "/Java.svg"],
+    link: "SC[Maruf_Ahmed]",
+  },
+  {
+    id: 3,
+    title: "9🎯 Portfolio Websitesr",
+    des: "Showcase work of individuals or companies, like artists, designers.",
+    img: "/3.svg",
+    iconLists: ["/re.svg", "/tail.svg", "/ts.svg", "/three.svg", "/c.svg"],
+    link: "SC[Jelena_Rebrnjak]",
+  },
+  {
+    id: 4,
+    title: "2🎯 Entertainment Websites",
+    des: "Provide videos, music, games, and other engaging content online.",
+    img: "/4.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "SC[Dwinawan]",
+  },
+  {
+    id: 5,
+    title: "3🎯 News Websites",
+    des: "Latest updates, news, and information about current events worldwide.",
+    img: "/5.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "SC[Happy_Tri_Milliarta]",
+  },
+  {
+    id: 6,
+    title: "5🎯 Educational Websites",
+    des: "Offer online courses, tutorials, educational resources, and learning materials.",
+    img: "/6.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "SC[Clickable_Agency]",
+  },
+  {
+    id: 7,
+    title: "7🎯 Nonprofit Websites",
+    des: "Promote missions and causes, provide information, and encourage donations.",
+    img: "/7.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "SC[Fahema_Yesmin]",
+  },
+  {
+    id: 8,
+    title: "1🎯 Social Media Websites",
+    des: "Platforms to connect, share content, communicate with other users.",
+    img: "/8.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "SC[Vektora]",
+  },
+  {
+    id: 9,
+    title: "16🎯Personal Websites",
+    des: "Individual pages sharing personal interests, experiences, stories, and hobbies.",
+    img: "/9.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "SC[Fahema_Yesmin]",
+  },
+  {
+    id: 10,
+    title: "2🎯 Forums Websites",
+    des: "Discussion platforms for sharing information and discussing various topics.",
+    img: "/10.svg",
+    iconLists: ["/next.svg", "/tail.svg", "/ts.svg", "/three.svg", "/gsap.svg"],
+    link: "SC[Madhu_Miah]",
+  },
+  {
+    id: 11,
+    title: "Eduwork",
+    des: "I manage and create content for various social media platforms.",
+    img: "/project5.svg",
+    iconLists: ["/excel.svg", "/spreadsheet.svg", "/igg.svg", "/tiktok.svg", "/lk.svg"],
+    link: "Eduwork",
+  },
+  {
+    id: 12,
+    title: "Crypto Enthusiast",
+    des: "As a Crypto Enthusiast, I am passionate about cryptocurrencies.",
+    img: "/project6.svg",
+    iconLists: ["/btc.svg", "/bnb.svg", "/xas.svg", "/idex.svg", "/usd.svg"],
+    link: "Binance",
+  },
+  {
+    id: 13,
+    title: "Collab ft PARAGON",
+    des: "Emina Beauty | December 2024",
+    img: "/project8.svg",
+    iconLists: ["/igg.svg", "/ttt.svg", "/x1.svg", "/gg.svg", "/mlogo.svg"],
+    link: "PARAGON",
+  },
+  {
+    id: 14,
+    title: "Collab ft PARAGON",
+    des: "Wardah Olympic | December 2024",
+    img: "/project9.svg",
+    iconLists: ["/igg.svg", "/ttt.svg", "/x1.svg", "/gg.svg", "/mlogo.svg"],
+    link: "PARAGON",
+  },
+  {
+    id: 15,
+    title: "Collab ft ICHITAN Indonesia",
+    des: "ICHITAN Indonesia | December 2024",
+    img: "/project10.svg",
+    iconLists: ["/igg.svg", "/ttt.svg", "/x1.svg", "/gg.svg", "/mlogo.svg"],
+    link: "ICHITAN Indonesia",
+  },
+  {
+    id: 16,
+    title: "Collab ft Rovers",
+    des: "Moko Moko Indonesia | December 2024",
+    img: "/project11.svg",
+    iconLists: ["/igg.svg", "/ttt.svg", "/x1.svg", "/gg.svg", "/mlogo.svg"],
+    link: "Moko Moko Indonesia",
+  },
+  {
+    id: 17,
+    title: "Founder Media Jakarta",
+    des: "1.2M Followers | 2.1M Followers | 612K Followers Instagram.",
+    img: "/project12.svg",
+    iconLists: ["/igehh.svg", "/ttt.svg", "/mlogo.svg"],
+    link: "Instagram",
+  },
+  {
+    id: 18,
+    title: "Founder ERHA",
+    des: "Which focuses on discussing Islam. Walaupun orangnya gak islam2 banget",
+    img: "/project13.svg",
+    iconLists: ["/igehh.svg", "/ttt.svg", "/mlogo.svg"],
+    link: "TikTok",
+  },
+  {
+    id: 19,
+    title: "Writter",
+    des: "Bukunya limited, cuma ada 1 di Jepang",
+    img: "/pro111.svg",
+    iconLists: ["/igehh.svg", "/ttt.svg", "/mlogo.svg"],
+    link: "Book",
+  },
+  {
+    id: 20,
+    title: "Collaboration Agromedia Group",
+    des: "Exclusive invitation from Gagasmedia for a podcast with other book authors.",
+    img: "/podcast.svg",
+    iconLists: ["/igehh.svg", "/ttt.svg", "/mlogo.svg"],
+    link: "Agromedia Group",
+  },
+  {
+    id: 21,
+    title: "Meta",
+    des: "The exclusive Meta Indonesia is intended for content creators, writers, and media professionals in Indonesia.",
+    img: "/meta.svg",
+    iconLists: ["/igehh.svg", "/ttt.svg", "/mlogo.svg"],
+    link: "Jakarta",
+  },
+  {
+    id: 22,
+    title: "Threads",
+    des: "The exclusive Meta Indonesia is intended for content creators, writers, and media professionals in Indonesia.",
+    img: "/project7.svg",
+    iconLists: ["/igehh.svg", "/ttt.svg", "/mlogo.svg"],
+    link: "Jakarta",
+  },
+
+  
+];
+export const projectss = [
+  {
+    id: 1,
+    title: "doain biar dapet loker",
+    des: "mang eak",
+    img: "/pro11.svg",
+    iconLists: ["/S1.svg", "/S2.svg", "/S3.svg", "/S2.svg", "/S4.svg"],
+    link: "mang eak",
+  },
+
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Alawi is amazing! He really knows website code, and the result made our website look great. Awesome.",
+    name: "Cheril",
+    title: "Student",
+  },
+  {
+    quote:
+      "Alawi graphic design makes my eyes happy. So creative! If you need design work, just go to him.",
+    name: "Audya Tsaani",
+    title: "Content Creator",
+  },
+  {
+    quote:
+      "Alawi is a pro at optimizing websites. After he helped, my website’s traffic skyrocketed. Thanks a bunch, bro!",
+    name: "Andika Pratama",
+    title: "Writer",
+  },
+  {
+    quote:
+      "Collaborating with Alawi is always cool. He responds quickly, comes up with creative solutions, and isn’t complicated. Top-notch!",
+    name: "Muhammad Afrizal",
+    title: "Writer",
+  },
+  {
+    quote:
+      "Recommend Alawi for web development? Absolutely! He doesn’t just code; he understands our business. The website turned out impressive and user-friendly!",
+    name: "Nanda",
+    title: "Writter",
+  },
+];
+
+export const companies = [
+  {
+    id: 1,
+    img: "/jnt.svg",
+  },
+  {
+    id: 2,
+    img: "/flowhite.svg",
+  },
+  {
+    id: 3,
+    img: "/jne.svg",
+  },
+  {
+    id: 4,
+    img: "/rel.svg",
+  },
+  {
+    id: 5,
+    img: "/kredivo.svg",
+  },
+  {
+    id: 6,
+    img: "/ricis.svg",
+  },
+  {
+    id: 7,
+    img: "/paragon.svg",
+  },
+  {
+    id: 8,
+    img: "/sman3.svg",
+  },
+  {
+    id: 9,
+    img: "/enlish.svg",
+  },
+  {
+    id: 10,
+    img: "/fmo.svg",
+  },
+  {
+    id: 11,
+    img: "/az.svg",
+  },
+];
+
+export const workExperience = [
+  {
+    id: 1,
+    title: "CEO Fokusind",
+    desc: "As the CEO of Fokusind, I lead and oversee the strategic direction of the company. My responsibilities include setting goals, managing resources, fostering growth, and ensuring that Fokusind continues to inspire and empower individuals in their self-development and career journeys.",
+    kerja: "Full Time | 2021 - Present",
+    className: "md:col-span-2",
+    thumbnail: "/work7.svg",
+  },
+  {
+    id: 2,
+    title: "Full Stack Developer",
+    kerja: "Pansus Studio | 9 Months",
+    desc: "As a full-stack developer, i handle both the front-end (user interface) and back-end (server-side logic) of web applications. I'm responsible for creating seamless user experiences, designing responsive interfaces, and implementing robust functionality.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/work8.svg",
+  },
+  {
+    id: 3,
+    title: "Programmer",
+    kerja: "Eduwork | 9 Months",
+    desc: "As a programmer, I write code to create software applications, solve problems, and automate tasks. My work involves designing algorithms, debugging code, and collaborating with other team members to build functional and efficient solutions.",
+    className: "md:col-span-2", // change to md:col-span-2
+    thumbnail: "/work9.svg",
+  },
+  {
+    id: 4,
+    title: "Social Media Specialist",
+    kerja: "Freelance | 2021 - Present",
+    desc: "As a social media specialist, I manage and optimize social media accounts for businesses or individuals. My responsibilities include creating engaging content, analyzing performance metrics, running ad campaigns, and fostering online.",
+    className: "md:col-span-2",
+    thumbnail: "/work6.svg",
+  },
+  {
+    id: 5,
+    title: "Front End Developer",
+    kerja: "Freelance Fiverr | 2021 - Present",
+    desc: "As a front-end developer, I role involves creating the visual elements of websites and web applications. I'm work with HTML, CSS, and JavaScript to build user interfaces that are interactive.",
+    className: "md:col-span-2",
+    thumbnail: "/work10.svg",
+  },
+  {
+    id: 6,
+    title: "Content Creator",
+    kerja: "Instagram & TikTok | 2021 - Present",
+    desc: "As a content creator, I role involves crafting engaging and relevant content for your audience. Whether I'm focus on writing articles, creating videos, managing a blog, or designing captivating graphics.",
+    className: "md:col-span-2",
+    thumbnail: "/work11.svg",
+  },
+];
+
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+  },
+];
